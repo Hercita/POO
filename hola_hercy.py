53b49a7abda94acfbfc19d8e98d898c29beca7a3
hercilia <hercylya@yandex.com>
2021-08-06 13:53:08 +0200

    confirmar cambios

--- hola_hercy.py
+++ hola_hercy.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 """
-Escribir script que pregunte el nombre del usuario en la consola y el numero entero e imprima
+Escribir SCRIPT que pregunte el nombre del usuario en la consola y un numero entero e imprima
 por pantalla en lineas distintas el nombre del usuario tantas veces como el numero
     
 """
 
-name = input("¿Como te llamas?")
-n = input("Introduce un  numero entero:")
-print ((name + "\n") * int(n))
+name = input("¿Como te llamas? ")  # input campo de entrada de datos, en python no se pone el punto y coma
+n = input("Introduce un  numero entero: ") # input campo de entrda de datos
+print ((name + "\n") * int(n))   #  /n salto de linea, * multiplicacion, con int decimos que n es entero
