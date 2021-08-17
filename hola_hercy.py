879d1e5462619579a5aec710bf3bc3ec6232de72
hercilia <hercylya@yandex.com>
2021-08-17 16:18:53 +0200

    probando a

--- hola_hercy.py
+++ hola_hercy.py
@@ -7,4 +7,5 @@ por pantalla en lineas distintas el nombre del usuario tantas veces como el nume
 
 name = input("¿Como te llamas? ")  # input campo de entrada de datos, en python no se pone el punto y coma
 n = input("Introduce un  numero entero: ") # input campo de entrda de datos
-print ((name + "\n") * int(n))   #  /n salto de linea, * multiplicacion, con int decimos que n es entero
+print ((name + "\n") * int(n))   #  /n es un salto de linea, * multiplicacion, con int decimos que n es entero
+  
